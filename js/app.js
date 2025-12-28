6d660c1ac8efd0c513c6f19c293b00d55aba1eaf
caserav <135666631+caserav@users.noreply.github.com>
2025-12-28 18:22:21 +0100

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 import { initAuth } from './auth.js';
 
 // Configuración obligatoria
-const SUPABASE_URL = 'https://rplieisbxvruijvnxbya.supabase.co';
-const SUPABASE_KEY = 'TU_ANON_KEY_AQUI'; // <--- Pon tu clave aquí
+const SUPABASE_URL = 'https://qpecqvvjyoycsxuvrzge.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFwZWNxdnZqeW95Y3N4dXZyemdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY4NDEyMTIsImV4cCI6MjA4MjQxNzIxMn0._NYiFimE45ATHyQEH1bE2PPebi7YvcMl5lylOXFxjEs';
 
 // Inicialización del cliente global
 // Lo ponemos en window para que sea accesible desde cualquier otro script (.js)
